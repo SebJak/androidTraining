758561f2f87cffce4378538ab63097362eb2b052
Sebastian J <sjak90@gmail.com>
2017-05-08 23:16:56 +0200

    Add maps and one marker

--- app/src/main/java/com/sjk/androidtraining/MainActivity.java
+++ app/src/main/java/com/sjk/androidtraining/MainActivity.java
@@ -1,13 +1,37 @@
 package com.sjk.androidtraining;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_main);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map){
+        Log.i("###", "on map ready");
+        double latitude = 52.5463, longitude = 19.7065;
+        googleMap = map;
+        MarkerOptions markerOptions = new MarkerOptions().position(new LatLng(latitude, longitude)).title("PLOCK");
+        googleMap.addMarker(markerOptions);
     }
 }
